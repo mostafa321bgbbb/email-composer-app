967131f32f0149994d1a2ba1d32255f5d45c8d7c
Mostafa Moharram <137335377+mostafa321bgbbb@users.noreply.github.com>
2024-01-02 15:09:10 -0500

    added mail tab

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { EmailComposer } from '@awesome-cordova-plugins/email-composer/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  providers: [EmailComposer]
 })
 export class Tab1Page {
-
-  constructor() {}
-
+  constructor(private emailComposer: EmailComposer) {}
+  emailOptions = {to: '', cc: '', bcc: '', subject: '', body: ''};
+  async openEmail() {
+    const email = {
+      to: this.emailOptions.to,
+      cc: this.emailOptions.cc,
+      bcc: this.emailOptions.bcc,
+      subject: this.emailOptions.subject,
+      body: this.emailOptions.body
+    };
+    await this.emailComposer.open(email);
+    console.log('opened');
+  }
 }
